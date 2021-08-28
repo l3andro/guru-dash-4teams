b2b36d6ccbf53bcec61d293dc4ea1cca4ed24021
Leandro Alves Morilha do Carmo <lmorilha@MMB026299CPS.local>
2021-08-28 12:18:47 -0300

    Fix Crontab By Provider

--- metrics-service/src/app.ts
+++ metrics-service/src/app.ts
@@ -1,16 +1,12 @@
 import { logger } from './shared/logger';
-import { schedule } from 'node-cron';
 import { syncMetrics } from './services/metrics.service';
-
-const cron = process.env.CRON || '* * * * *';
-logger.info(`Scheduling next execution with expression: ${cron}`);
 
 async function main() {
   try {
     await syncMetrics();
   } catch (err) {
-    logger.error(err, 'Error running process');
+    logger.error('Error running process');
   }
 }
 
-main().then(() => schedule(cron, main));
+main().then(() => (main));
